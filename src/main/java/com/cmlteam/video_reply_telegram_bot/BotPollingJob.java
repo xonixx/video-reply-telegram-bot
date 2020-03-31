112349bdfca0eca5132dad8c31129130fffbd13c
xonix <xonixx@gmail.com>
2020-03-31 23:59:54 +0300

    Endpoint to send video that returns its ID #2

--- src/main/java/com/cmlteam/video_reply_telegram_bot/BotPollingJob.java
+++ src/main/java/com/cmlteam/video_reply_telegram_bot/BotPollingJob.java
@@ -1,9 +1,14 @@
 package com.cmlteam.video_reply_telegram_bot;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.model.Message;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.model.Video;
 import com.pengrad.telegrambot.request.GetUpdates;
 import com.pengrad.telegrambot.request.SendMessage;
+import com.pengrad.telegrambot.request.SendVideo;
 import com.pengrad.telegrambot.response.GetUpdatesResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -27,11 +32,25 @@ public class BotPollingJob {
     List<Update> updates = updatesResponse.updates();
 
     for (Update update : updates) {
-      log.info("Received: " + update);
-      
-      telegramBot.execute(new SendMessage(update.message().chat().id(), "" + update.updateId()));
+      log.info("Received:\n" + toPrettyString(update));
+
+      Message message = update.message();
+      //      telegramBot.execute(new SendMessage(message.chat().id(), "" + update.updateId()));
+
+      Video video = message.video();
+      if (video != null) {
+        String fileId = video.fileId();
+        //        telegramBot.execute(new SendVideo(message.chat().id(), fileId).caption(fileId));
+        telegramBot.execute(new SendMessage(message.chat().id(), fileId));
+      }
 
       getUpdates.offset(update.updateId() + 1);
     }
+  }
+
+  private final Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+  private String toPrettyString(Object obj) {
+    return gson.toJson(obj);
   }
 }
