23c7d2460fc2c0dd56b8f3b520a18db7a25353ae
xonix <xonixx@gmail.com>
2020-03-31 23:34:59 +0300

    Start with polling bot (no webhook) #3

--- src/main/java/com/cmlteam/video_reply_telegram_bot/BotPollingJob.java
+++ src/main/java/com/cmlteam/video_reply_telegram_bot/BotPollingJob.java
@@ -1,15 +1,37 @@
 package com.cmlteam.video_reply_telegram_bot;
 
 import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.GetUpdates;
+import com.pengrad.telegrambot.request.SendMessage;
+import com.pengrad.telegrambot.response.GetUpdatesResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
+@RequiredArgsConstructor
+@Slf4j
 public class BotPollingJob {
-  private TelegramBot telegramBot;
+  private final TelegramBot telegramBot;
+
+  private final GetUpdates getUpdates = new GetUpdates();
 
   @Scheduled(fixedRate = 400)
   public void processUpdates() {
-    System.out.println("processUpdates...");
+    GetUpdatesResponse updatesResponse = telegramBot.execute(getUpdates);
+
+    List<Update> updates = updatesResponse.updates();
+
+    for (Update update : updates) {
+      log.info("Received: " + update);
+      
+      telegramBot.execute(new SendMessage(update.message().chat().id(), "" + update.updateId()));
+
+      getUpdates.offset(update.updateId() + 1);
+    }
   }
 }
