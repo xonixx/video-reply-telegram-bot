dce5fda2b47d4ce428d55d084ad6b70fb8d2764e
xonix <xonixx@gmail.com>
2020-04-02 17:12:18 +0300

    fix deployment

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,11 @@ APP=video_reply_telegram_bot
 USER=apps1
 SERV=prod.cmlteam.com
 
+if [[ -z "$TELEGRAM_BOT_ADMIN_USER" || -z "$TELEGRAM_BOT_TOKEN" ]] ; then
+    echo "You must set TELEGRAM_BOT_ADMIN_USER and TELEGRAM_BOT_TOKEN env. vars" >&2
+    exit 1
+fi
+
 echo
 echo "BUILD..."
 echo
@@ -26,7 +31,10 @@ then
     sudo ln -s /home/$USER/$APP.jar /etc/init.d/$APP
     sudo update-rc.d $APP defaults 99
 fi
-sudo /etc/init.d/$APP restart
+sudo \
+    TELEGRAM_BOT_ADMIN_USER=$TELEGRAM_BOT_ADMIN_USER \
+    TELEGRAM_BOT_TOKEN=$TELEGRAM_BOT_TOKEN \
+    /etc/init.d/$APP restart
 sleep 20
-tail -n 100 /var/log/$APP.log
+tail -n 200 /var/log/$APP.log
 "
