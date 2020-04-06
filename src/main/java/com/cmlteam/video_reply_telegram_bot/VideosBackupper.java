84189ffc9ef90c7c8e2289ff1a87b927559c2227
xonix <xonixx@gmail.com>
2020-04-06 03:45:25 +0300

    Backup videos to local folder #11

--- src/main/java/com/cmlteam/video_reply_telegram_bot/VideosBackupper.java
+++ src/main/java/com/cmlteam/video_reply_telegram_bot/VideosBackupper.java
@@ -6,9 +6,13 @@ import com.pengrad.telegrambot.request.GetFile;
 import com.pengrad.telegrambot.request.SendMessage;
 import com.pengrad.telegrambot.response.GetFileResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FileUtils;
 import org.springframework.scheduling.annotation.Async;
 
+import javax.annotation.PostConstruct;
+import java.net.URL;
 import java.util.List;
 
 @Slf4j
@@ -19,6 +23,13 @@ public class VideosBackupper {
   private final TelegramBotWrapper telegramBot;
   private final VideosListProperties videosListProperties;
 
+  @PostConstruct
+  void postConstruct() {
+    if (!new java.io.File(backupFolder).isDirectory()) {
+      throw new IllegalArgumentException("Not a folder: " + backupFolder);
+    }
+  }
+
   @Async
   public void startBackup(long userToInform) {
     List<Video> videos = videosListProperties.getList();
@@ -27,14 +38,20 @@ public class VideosBackupper {
 
     long t0 = System.currentTimeMillis();
 
-    for (Video video : videos) {
-      backupVideo(video);
+    try {
+      for (Video video : videos) {
+        backupVideo(video);
+      }
+    } catch (Exception ex) {
+      log.error("", ex);
+      telegramBot.execute(new SendMessage(userToInform, "Exception: " + ex.toString()));
     }
 
     telegramBot.execute(
         new SendMessage(userToInform, "Done in " + Util.renderDurationFromStart(t0)));
   }
 
+  @SneakyThrows
   private void backupVideo(Video video) {
     GetFileResponse fileResponse = telegramBot.execute(new GetFile(video.getFileId()));
 
@@ -42,7 +59,15 @@ public class VideosBackupper {
 
     String filePath = file.filePath();
 
-    log.info(formFileDlUrl(filePath));
+    String videoUrl = formFileDlUrl(filePath);
+
+    log.info("Downloading {} : {}... ", video.getFileId(), videoUrl);
+
+    FileUtils.copyURLToFile(
+        new URL(videoUrl),
+        new java.io.File(backupFolder, video.getFileUniqueId() + ".mp4"),
+        10_000,
+        10_000);
   }
 
   private String formFileDlUrl(String filePath) {
