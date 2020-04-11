52c55bac5b363b7a3361336caef8fe817be635bf
xonix <xonixx@gmail.com>
2020-04-11 21:38:57 +0300

    Search even when entered in wrong keyboard layout #13

--- src/main/java/com/cmlteam/video_reply_telegram_bot/SearchStringMatcher.java
+++ src/main/java/com/cmlteam/video_reply_telegram_bot/SearchStringMatcher.java
@@ -1,15 +1,27 @@
 package com.cmlteam.video_reply_telegram_bot;
 
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
+
+import static org.apache.commons.lang3.StringUtils.replaceChars;
 
 @Component
 public class SearchStringMatcher {
+
+  private final String RU_KEYBOARD = "йцукенгшщзхъфывапролджэячсмитьбюё";
+  private final String EN_KEYBOARD = "qwertyuiop[]asdfghjkl;'zxcvbnm,.`";
 
   public boolean matches(String keyword, String query) {
     String keywordNormalized = normalize(keyword);
     String queryNormalized = normalize(query);
 
+    return matches0(keywordNormalized, queryNormalized)
+        || matches0(
+            keywordNormalized, normalize(replaceChars(queryNormalized, EN_KEYBOARD, RU_KEYBOARD)))
+        || matches0(
+            keywordNormalized, normalize(replaceChars(queryNormalized, RU_KEYBOARD, EN_KEYBOARD)));
+  }
+
+  private boolean matches0(String keywordNormalized, String queryNormalized) {
     return queryNormalized.contains(keywordNormalized)
         || keywordNormalized.contains(queryNormalized);
   }
@@ -18,6 +30,6 @@ public class SearchStringMatcher {
     if (str == null) {
       return null;
     }
-    return StringUtils.replaceChars(str.toLowerCase(), "ё", "е");
+    return replaceChars(str.toLowerCase(), "ё", "е");
   }
 }
--- src/test/java/com/cmlteam/video_reply_telegram_bot/SearchStringMatcherTest.java
+++ src/test/java/com/cmlteam/video_reply_telegram_bot/SearchStringMatcherTest.java
@@ -8,12 +8,17 @@ public class SearchStringMatcherTest {
   private final SearchStringMatcher searchStringMatcher = new SearchStringMatcher();
 
   @Test
-  void tests() {
+  void testMatchesEmpty() {
+    assertTrue(searchStringMatcher.matches("наши полномочия все окончены", ""));
+  }
+
+  @Test
+  void basicTests() {
     assertTrue(searchStringMatcher.matches("наши полномочия все окончены", "полномочия"));
     assertTrue(searchStringMatcher.matches("наши полномочия все окончены", "полномочия всё"));
     assertTrue(searchStringMatcher.matches("наши полномочия все окончены", "полномочия все"));
     assertTrue(searchStringMatcher.matches("наши полномочия все окончены", "полном"));
-    
+
     assertTrue(searchStringMatcher.matches("наши полномочия всё окончены", "полномочия"));
     assertTrue(searchStringMatcher.matches("наши полномочия всё окончены", "полномочия всё"));
     assertTrue(searchStringMatcher.matches("наши полномочия всё окончены", "полномочия все"));
@@ -23,5 +28,13 @@ public class SearchStringMatcherTest {
     assertTrue(searchStringMatcher.matches("наши полномочия всё окончены", "полномочия всЁ"));
     assertTrue(searchStringMatcher.matches("наши полномочия всё окончены", "ПОЛНомочия все"));
     assertTrue(searchStringMatcher.matches("наши полномочия всё окончены", "ПОЛНом"));
+  }
+
+  @Test
+  void testMatchesWrongKeyboard() {
+    assertTrue(searchStringMatcher.matches("наши полномочия все окончены", "gjkyjvjxbz"));
+    assertTrue(searchStringMatcher.matches("наши полномочия все окончены", "gjKYjVjXBz"));
+    assertTrue(searchStringMatcher.matches("наши полномочия все окончены", "gjKYjVjXBz DCt"));
+    assertTrue(searchStringMatcher.matches("wtf", "цеа"));
   }
 }
