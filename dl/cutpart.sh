92e6f25acbd24c5460b27f45d4e099c192dabfec
xonix <xonixx@gmail.com>
2020-06-05 03:36:24 +0300

    improve positioning while cutting videos

---
+++ dl/cutpart.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+file=$1
+start=$2
+duration=$3
+
+file_out=${file/.mp4/_cut.mp4}
+#file_out1=${file/.mp4/_cut1.mp4}
+#file_out2=${file/.mp4/_cut2.mp4}
+
+#yes | ffmpeg -i "$file" -c copy "$file_out"
+#yes | ffmpeg -i "$file_out"  -acodec copy -c:v libx264 -x264opts keyint=1 "$file_out1"
+#yes | ffmpeg -i "$file_out1" -acodec copy -c:v libx264 -x264opts keyint=100 "$file_out2"
+
+yes | ffmpeg -ss "$start" -i "$file" -t "$duration" -acodec copy -c:v libx264 -x264opts keyint=100 "$file_out"
+
+echo $file_out
+
+
