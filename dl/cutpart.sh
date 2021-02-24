0d2ae5cb0b61e8933ea3b386753418dbc93fafdd
xonix <xonixx@gmail.com>
2021-02-24 16:29:28 +0200

    cutpart.sh : nosound option

--- dl/cutpart.sh
+++ dl/cutpart.sh
@@ -3,6 +3,14 @@
 file=$1
 start=$2
 duration=$3
+nosound=1
+
+if (( 1 == nosound ))
+then
+  ffmpeg_cmd="-vcodec copy -filter:a volume=0"
+else
+  ffmpeg_cmd="-c copy"
+fi
 
 file_out=${file/.mp4/_cut.mp4}
 
@@ -13,7 +21,7 @@ else
   duration_cmd="-t $duration"
 fi
 
-yes | ffmpeg -ss "$start" -i "$file" $duration_cmd  -c copy "$file_out"
+yes | ffmpeg -ss "$start" -i "$file" $duration_cmd $ffmpeg_cmd "$file_out"
 
 echo $file_out
 
