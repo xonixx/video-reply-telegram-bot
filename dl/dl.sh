2d18bf0d9ef9aa174a6119cfdbffd34d55a4c5b9
xonix <xonixx@gmail.com>
2021-09-14 16:32:35 +0300

    YTDL

--- dl/dl.sh
+++ dl/dl.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
+#YTDL=youtube-dl
+YTDL="python39 /usr/local/bin/yt-dlp"
+
 (( i = 0 ))
 for f in $(cat 1.txt) ; do
     (( i++ ))
     echo
     echo "Downloading #$i: $f"
     echo
-    youtube-dl -o "./vids/${i}_%(title)s.%(ext)s" $f -f 18
+    $YTDL -o "./vids/${i}_%(title)s.%(ext)s" $f -f 18
 done
